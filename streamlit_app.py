a2995c8815b02470a4d3e11970d09fff26acb777
John Hanley <jhanley708-git@s6.sector6.net>
2021-11-02 17:33:44 -0700

    Lint

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
+
 from collections import namedtuple
-import altair as alt
 import math
+
+import altair as alt
 import pandas as pd
 import streamlit as st
 
@@ -14,7 +16,6 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
@@ -34,5 +35,5 @@ with st.echo(code_location='below'):
         data.append(Point(x, y))
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+                    .mark_circle(color='#0068c9', opacity=0.5)
+                    .encode(x='x:Q', y='y:Q'))
